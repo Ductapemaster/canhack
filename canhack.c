0f7b2a2c49cfa800e76c3ee32788e1f92707f5e3
Ductapemaster <Ductapemaster@gmail.com>
2015-08-09 13:51:19 -0700

    Updating copyright

--- canhack.c
+++ canhack.c
@@ -1,5 +1,6 @@
 /*
- * Copyright 2013 Fabio Baltieri <fabio.baltieri@gmail.com>
+ * Original work copyright 2013 Fabio Baltieri <fabio.baltieri@gmail.com>
+ * Modified work copyright 2015 Dan Kouba
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
